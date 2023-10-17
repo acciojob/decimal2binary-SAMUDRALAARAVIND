98ec3a7d150a75124b7f160508c339ba4b9643e1
SAMUDRALA ARAVIND <67514829+SAMUDRALAARAVIND@users.noreply.github.com>
2023-10-17 14:16:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function decimalToBinary(num) {
-  // Write your code here
+	let binary = "" ;
+	while(num >= 2) {
+		binary =  (num % 2) + binary;
+		num = parseInt(num / 2);
+	}
+	if(num === 1) {
+		return num + binary;
+	}
+	return binary ;
 }
 
 window.decimalToBinary = decimalToBinary;
